f6b5e7d440f6b0abd2da0dccdf0818802a1d4173
Randy Syring <rsyring@gmail.com>
2008-11-16 06:47:50 +0000

    routing.py: add different lookup for url so we can get the full url to use in <base>

--- pysmvt/routing.py
+++ pysmvt/routing.py
@@ -1,5 +1,7 @@
 from pysmvt import settings, rg
-from werkzeug.routing import Rule
+from werkzeug.routing import Rule, RequestRedirect
+from werkzeug.exceptions import NotFound, MethodNotAllowed
+from pysmvt.exceptions import SettingsError, ProgrammingError
 
 __all__ = [
     'Rule',
@@ -21,10 +23,22 @@ def js_url(file, app = None):
     endpoint = 'javascript'
     return url_for(endpoint, file=file, app=app)
 
-def index_url(url = None):
-    if settings.routing.prefix:
-       return '/%s/' % settings.routing.prefix.strip('/')
-    return '/'
+def index_url(full=False):
+    
+    try:
+        if settings.routing.prefix:
+            url = '/%s/' % settings.routing.prefix.strip('/')
+        else:
+            url = '/'
+        
+        endpoint, args = rg.urladapter.match( url )
+        return url_for(endpoint, _external=full, **args)
+    except NotFound:
+        raise SettingsError('the index url "%s" could not be located' % url)
+    except MethodNotAllowed :
+        raise ProgrammingError('index_url(): MethodNotAllowed exception encountered')
+    except RequestRedirect:
+        raise SettingsError('the index url "%s" generated a redirect' % url)
 
 def add_prefix(path):
     if settings.routing.prefix:
