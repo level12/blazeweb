e829159c61936b3d840aeaa5e3c3e728ae5db840
Randy Syring <rsyring@gmail.com>
2008-12-16 06:11:51 +0000

    - setup.py: using find_packages and setting long_description and __doc__

--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+"""
+Introduction
+---------------
+
+pysmvt is a wsgi web framework library designed in the spirit of pylons but with
+django modularity (i.e. what they would call "apps").  If you want to try it out
+it would be best to start with `pysapp <http://pypi.python.org/pypi/pysapp/>`_
+which is a pysmvt "supporting application" that your projects can be built on.
+
+Current Status
+---------------
+
+We are currently in an alpha phase which means lots of stuff can change, maybe rapidly, and we are not interested in backwards compatibility at this point.
+
+I am currently using this library for some production websites, but I wouldn't recommend you do that unless you **really** know what you are doing.
+
+The unstable `development version
+<https://svn.rcslocal.com:8443/svn/pysmvt/pysmvt/trunk#egg=pysmvt-dev>`_.
+"""
 import sys
 try:
     from setuptools import setup, find_packages
@@ -8,18 +27,19 @@ except ImportError:
 
 setup(
     name = "pysmvt",
-    version = "0.1.0",
-    description = "A wsgi compliant web framework",
+    version = "0.1dev",
+    description = "A wsgi web framework with a pylons spirit and django modularity",
+    long_description = __doc__,
     author = "Randy Syring",
     author_email = "randy@rcs-comp.com",
-    url='http://pypi.python.org/pypi/pysmvt',
+    url='http://pypi.python.org/pypi/pysmvt/',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
       ],
     license='BSD',
-    packages=['pysmvt'],
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     install_requires = [
         "Beaker>=1.1.2",
         "FormEncode>=1.2",
