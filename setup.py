f47458d8cc2a9ddf654c367c56790898d72de303
randy <unknown>
2009-05-04 19:02:29 +0000

    Added package files

---
+++ setup.py
@@ -0,0 +1,29 @@
+import sys
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name = "pysmvttestapp",
+    version = "0.1dev",
+    description = "pysmvt applications that support the pysmvt tests",
+    long_description = __doc__,
+    author = "Randy Syring",
+    author_email = "randy@rcs-comp.com",
+    url='http://pypi.python.org/pypi/pysmvt/',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+      ],
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    install_requires = [
+        "pysmvt>=dev"
+    ],
+    zip_safe=False
+)
