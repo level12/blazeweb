b2f2727820b204a6c8c9631759805f92aa10e388
Randy Syring <rsyring@gmail.com>
2009-05-06 21:37:19 +0000

    added test for wrapinapp()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,13 @@
-import unittest
-import config 
+from pysmvt.routing import current_url
+from pysmvt.utils import wrapinapp
 
-        
-if __name__ == '__main__':
-    unittest.main()
+# create the wsgi application that will be used for testing
+from pysmvttestapp.applications import make_wsgi
+app = make_wsgi('Testruns')
+
+# call test_currenturl() inside of a working wsgi app.  current_url()
+# depends on a correct environment being setup and would not work
+# otherwise.
+@wrapinapp(app)
+def test_currenturl():
+    assert current_url(host_only=True) == 'http://localhost/'
