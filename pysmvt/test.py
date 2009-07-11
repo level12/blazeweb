b7c4be2cdaba71a9e9941a72544a450b26aacd98
Randy Syring <rsyring@gmail.com>
2009-07-11 20:16:13 +0000

    Formatting adjustments

--- pysmvt/test.py
+++ pysmvt/test.py
@@ -1,112 +1,111 @@
-    """
-        == Making The Plugins Available ==
+"""
+    == Making The Plugins Available ==
+    
+    Make sure something like the following is in your application packages
+    setup.py file:
+    
+        entry_points=\"""
+                # -*- Entry points: -*-
+                [nose.plugins]
+                testsfrompackage = pysmvt.test:TestsFromPackagePlugin
+                initcurrentapp = pysmvt.test:InitCurrentAppPlugin
+            \""",
+    
+    Then either install the package or run `python setup.py develop`.
+    Assuming nose is installed, you should then be able to run:
         
-        Make sure something like the following is in your application packages
-        setup.py file:
+        `nosetests --help`
+    
+    and you should see the following in the output:
+    
+        ...
+        --add-pkg-tests=PYSMVT_PKGTESTS
+                    A comma separated list of packages from which tests
+                    should be gathered
+        --app-profile=PYSMVT_PROFILE
+                    The name of the test profile in settings.py
+        ...
+    
+    == Using the Plugins ==
+    
+    You **must** be inside a pysmvt application's package directory for
+    these plugins to work:
+    
+        `cd .../myproject/src/myapp-dist/myapp/`
+    
+    === Tests From Package Plugin ===
+    
+    Allows one to run tests from another package. Useful when you are
+    running tests for an application but also want to run tests on
+    supporting applications that are not under the current working
+    directory. For example, in order to run the tests for pysapp, from the
+    pysappexample application (because an application is required for some
+    of the pysapp functional tests), you would run:
+    
+        `cd .../myproject/src/pysappexample-dist/pysappexample/`
+        `nosetests --add-pkg-tests=pysapp`
+    
+    This will run all the tests in pysappexample as well as pysapp. If you
+    are going to do this kind of thing a lot, there is an easier way.  See
+    the next section.
+    
+    === Init Current App Plugin ===
+    
+    This plugin does two things:
         
-            entry_points=\"""
-                    # -*- Entry points: -*-
-                    [nose.plugins]
-                    testsfrompackage = pysmvt.test:TestsFromPackagePlugin
-                    initcurrentapp = pysmvt.test:InitCurrentAppPlugin
-                \""",
+        - initializes a WSGI application for the current application
+          (optionally allowing you to specify which profile you want used
+          to initlize the application)
+        - automatically includes test's from packages if so defined in the
+          profile which is loaded.
+    
+    You don't have to do anything explicit to use this plugin.  It is
+    enabled automatically when `nosetests` is run from inside an
+    application's directory structure.  Assuming your make_wsgi() function
+    is setup correctly, globaly proxy objects like 'ag' should now function
+    correctly.  Request level objects, like 'rg' will not yet be available
+    however.
+    
+    In order to get access to the wsgi application that was instantiated,
+    you can do:
         
-        Then either install the package or run `python setup.py develop`.
-        Assuming nose is installed, you should then be able to run:
-            
-            `nosetests --help`
+        from pysmvt import ag
         
-        and you should see the following in the output:
+        testapp = ag._wsgi_test_app
         
-            ...
-            --add-pkg-tests=PYSMVT_PKGTESTS
-                        A comma separated list of packages from which tests
-                        should be gathered
-            --app-profile=PYSMVT_PROFILE
-                        The name of the test profile in settings.py
-            ...
+    `testapp` could now be used in the pysmvt.utils.wrapinapp() decorator.
+    
+    The default profile used with this plugin is 'Test'.  If you need to
+    specifiy a different profile, do:
         
-        == Using the Plugins ==
+        `nosetests  --app-profile=mytestprofile`
         
-        You **must** be inside a pysmvt application's package directory for
-        these plugins to work:
+    To include tests from packges outside the application's directory
+    structure, you can put a `testing.include_pkgs` attribute in your test
+    profile. For example:
+    
+        class TestPysapp(Test):
+            def __init__(self):
+                # call parent init to setup default settings
+                Test.__init__(self)
+                
+                # include pysapp tests
+                self.testing.include_pkgs = 'pysapp'
+        testpysapp = TestPysapp
+    
+    Running:
         
-            `cd .../myproject/src/myapp-dist/myapp/`
+        `nosetests  --app-profile=testpysapp`
+    
+    Would be equivelent to running:
+    
+        `nosetests --add-pkg-tests=pysapp`
+    
+    Packages can also be specified as a list/tuple:
         
-        === Tests From Package Plugin ===
-        
-        Allows one to run tests from another package. Useful when you are
-        running tests for an application but also want to run tests on
-        supporting applications that are not under the current working
-        directory. For example, in order to run the tests for pysapp, from the
-        pysappexample application (because an application is required for some
-        of the pysapp functional tests), you would run:
-        
-            `cd .../myproject/src/pysappexample-dist/pysappexample/`
-            `nosetests --add-pkg-tests=pysapp`
-        
-        This will run all the tests in pysappexample as well as pysapp. If you
-        are going to do this kind of thing a lot, there is an easier way.  See
-        the next section.
-        
-        === Init Current App Plugin ===
-        
-        This plugin does two things:
-            
-            - initializes a WSGI application for the current application
-              (optionally allowing you to specify which profile you want used
-              to initlize the application)
-            - automatically includes test's from packages if so defined in the
-              profile which is loaded.
-        
-        You don't have to do anything explicit to use this plugin.  It is
-        enabled automatically when `nosetests` is run from inside an
-        application's directory structure.  Assuming your make_wsgi() function
-        is setup correctly, globaly proxy objects like 'ag' should now function
-        correctly.  Request level objects, like 'rg' will not yet be available
-        however.
-        
-        In order to get access to the wsgi application that was instantiated,
-        you can do:
-            
-            from pysmvt import ag
-            
-            testapp = ag._wsgi_test_app
-            
-        `testapp` could now be used in the pysmvt.utils.wrapinapp() decorator.
-        
-        The default profile used with this plugin is 'Test'.  If you need to
-        specifiy a different profile, do:
-            
-            `nosetests  --app-profile=mytestprofile`
-            
-        To include tests from packges outside the application's directory
-        structure, you can put a `testing.include_pkgs` attribute in your test
-        profile. For example:
-        
-            class TestPysapp(Test):
-                def __init__(self):
-                    # call parent init to setup default settings
-                    Test.__init__(self)
-                    
-                    # include pysapp tests
-                    self.testing.include_pkgs = 'pysapp'
-            testpysapp = TestPysapp
-        
-        Running:
-            
-            `nosetests  --app-profile=testpysapp`
-        
-        Would be equivelent to running:
-        
-            `nosetests --add-pkg-tests=pysapp`
-        
-        Packages can also be specified as a list/tuple:
-            
-            # include multiple tests
-            self.testing.include_pkgs = ('pysapp', 'somepkg')
-        
-    """
+        # include multiple tests
+        self.testing.include_pkgs = ('pysapp', 'somepkg')        
+"""
 
 import os
 import nose.plugins
