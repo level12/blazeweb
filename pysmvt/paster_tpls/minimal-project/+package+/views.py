0aa357cbdbe6097fb830c312e6d4017d2d75bd85
Randy Syring <rsyring@gmail.com>
2010-06-05 00:40:59 -0400

    files that go with the last commit

---
+++ pysmvt/paster_tpls/minimal-project/+package+/views.py
@@ -0,0 +1,11 @@
+from pysmvt.view import asview
+
+@asview('/')
+def index():
+    return 'index'
+
+@asview()
+def broke():
+    # this is here so you can see the email/logging that happens if your
+    # app has an exception
+    foo = bar
