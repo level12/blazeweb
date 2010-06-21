06231139b1fadc9c2e850c00ac51d0ddfb7a9586
Randy Syring <rsyring@gmail.com>
2010-06-21 17:10:20 -0400

    fixed failing test, clear *.pyc files when moving stuff :)

--- tests/apps/minimal2/application.py
+++ tests/apps/minimal2/application.py
@@ -2,7 +2,7 @@ from os import path
 from blazeutils import prependsitedir
 from blazeweb.application import WSGIApp
 from blazeweb.middleware import full_wsgi_stack
-import settings as settingsmod
+import config.settings as settingsmod
 from blazeweb.scripting import application_entry
 
 # make sure our base module gets put on the path
